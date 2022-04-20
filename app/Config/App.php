70d272c1d26a4ed91bdc7997697061a9692582c8
AjengKurniawati <100357428+AjengKurniawati@users.noreply.github.com>
2022-04-20 17:32:49 +0700

    update app

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/tugaspemprog6/';
+    public $baseURL = 'http://localhost/pemograman6/';
 
     /**
      * --------------------------------------------------------------------------
